45e8a7bb2eab66a9b5aafcb7046fb71fa9f31d90
Aliaksandra <alekspekhota@gmail.com>
2025-05-15 11:39:17 +0200

    init: add ts config eslint prettier

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -4,6 +4,7 @@ import * as http from 'http';
 
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
+
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
     fs.readFile(file_path, function (err, data) {
         if (err) {
