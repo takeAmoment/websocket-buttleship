4072dde7a668e053c96c66d6b81dcc338ff47b2c
Aliaksandra <alekspekhota@gmail.com>
2025-05-15 11:44:25 +0200

    feat: add port to env file

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config';
+
 import { httpServer } from './src/http_server/index';
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = process.env.HTTP_PORT;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
